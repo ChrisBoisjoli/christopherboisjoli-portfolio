1e84036b1b458efcb53588cecf6c536b0b1f9da0
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-05 15:40:17 -0500

    corrected formatting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,49 +1,53 @@
 import React from "react";
-function Home(){
-    return (<div class="container">
-    <div class="row">
-      <div class="card col-md-8 animate__animated animate__backInDown">
-        <img
-          src="./assets/headshot.jpg"
-          class="card-img-top headshot"
-          alt="Photo of developer Chris Boisjoli"
-        />
-        <div class="card-body">
-          <h5 class="card-title">About Me</h5>
-          <a href="https://github.com/ChrisBoisjoli" target="_blank"
-            ><i class="contact fab fa-github fa-2x"></i
-          ></a>
-          <a
-            href="https://www.linkedin.com/in/chris-boisjoli-b13193200/"
-            target="_blank"
-            ><i class="contact fab fa-linkedin fa-2x"></i
-          ></a>
-          <a href="mailto:christopherboisjoli@gmail.com"
-            ><i class="contact fas fa-envelope-square fa-2x"></i
-          ></a>
-          <a href="tel:612-460-0187"
-            ><i class="contact fas fa-phone fa-2x"></i
-          ></a>
-          <a
-            href="./assets/Christopher_Boisjoli_Resume2_11.pdf"
-            target="_blank"
-            ><i class="contact fas fa-file fa-2x"></i
-          ></a>
-          <p class="card-text">
-            Hi! My name is Chris Boisjoli and I am in the UMN Coding Bootcamp
-            learning the MERN stack. So far in the course we have learned
-            HTML, CSS, JavaScript, Node, Express, MySQL and I am very excited
-            to learn more. I am 35 years old and have been married to my wife
-            Christine for 13 years. We live in Minneapolis with our dog
-            Dottie. I am originally from Duluth MN and graduated with a degree
-            in International Relations from St. Cloud State University where I
-            participated in ROTC and spent 10 years in the Army National
-            Guard.
-          </p>
+function Home() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="card col-md-8 animate__animated animate__backInDown">
+          <img
+            src="./assets/headshot.jpg"
+            class="card-img-top headshot"
+            alt="Photo of developer Chris Boisjoli"
+          />
+          <div class="card-body">
+            <h5 class="card-title">About Me</h5>
+            <a href="https://github.com/ChrisBoisjoli" target="_blank">
+              <i class="contact fab fa-github fa-2x"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/chris-boisjoli-b13193200/"
+              target="_blank"
+            >
+              <i class="contact fab fa-linkedin fa-2x"></i>
+            </a>
+            <a href="mailto:christopherboisjoli@gmail.com">
+              <i class="contact fas fa-envelope-square fa-2x"></i>
+            </a>
+            <a href="tel:612-460-0187">
+              <i class="contact fas fa-phone fa-2x"></i>
+            </a>
+            <a
+              href="./assets/Christopher_Boisjoli_Resume2_11.pdf"
+              target="_blank"
+            >
+              <i class="contact fas fa-file fa-2x"></i>
+            </a>
+            <p class="card-text">
+              Hi! My name is Chris Boisjoli and I am in the UMN Coding Bootcamp
+              learning the MERN stack. So far in the course we have learned
+              HTML, CSS, JavaScript, Node, Express, MySQL and I am very excited
+              to learn more. I am 35 years old and have been married to my wife
+              Christine for 13 years. We live in Minneapolis with our dog
+              Dottie. I am originally from Duluth MN and graduated with a degree
+              in International Relations from St. Cloud State University where I
+              participated in ROTC and spent 10 years in the Army National
+              Guard.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-    </div>);
+  );
 }
 
 export default Home;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,65 +1,72 @@
 import React from "react";
-function Portfolio(){
-    return (<div class="container">
-    <div class="row">
-      <div class="animate__animated animate__lightSpeedInRight card col-md-3">
-        <img src="./assets/reel2real.png" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Reel2Real</h5>
-          <a href="https://github.com/ChrisBoisjoli/reel2real" target="_blank"
-            ><i class="live fab fa-github fa-3x"></i
-          ></a>
-          <p class="card-text">
-            This is the second project that I worked on. I was responsible for
-            many of the MVC back-end routes.
-          </p>
+function Portfolio() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="animate__animated animate__lightSpeedInRight card col-md-3">
+          <img src="./assets/reel2real.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Reel2Real</h5>
+            <a
+              href="https://github.com/ChrisBoisjoli/reel2real"
+              target="_blank"
+            >
+              <i class="live fab fa-github fa-3x"></i>
+            </a>
+            <p class="card-text">
+              This is the second project that I worked on. I was responsible for
+              many of the MVC back-end routes.
+            </p>
+          </div>
         </div>
-      </div>
-      <div class="animate__animated animate__lightSpeedInRight card col-md-3">
-        <img src="./assets/backroom.png" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Backroom</h5>
-          <a href="https://github.com/ChrisBoisjoli/Backroom" target="_blank"
-            ><i class="live fab fa-github fa-3x"></i
-          ></a>
-          <p class="card-text">
-            With this project I created a database server for an E-Commerce site
-            to update their backroom storage.
-          </p>
+        <div class="animate__animated animate__lightSpeedInRight card col-md-3">
+          <img src="./assets/backroom.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Backroom</h5>
+            <a href="https://github.com/ChrisBoisjoli/Backroom" target="_blank">
+              <i class="live fab fa-github fa-3x"></i>
+            </a>
+            <p class="card-text">
+              With this project I created a database server for an E-Commerce
+              site to update their backroom storage.
+            </p>
+          </div>
         </div>
-      </div>
-      <div class="animate__animated animate__lightSpeedInRight card col-md-3">
-        <img src="./assets/teamprofile.png" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Profie Generator</h5>
-          <a
-            href="https://github.com/ChrisBoisjoli/Team_Profile_Generator"
-            target="_blank"
-            ><i class="live fab fa-github fa-3x"></i
-          ></a>
-          <p class="card-text">
-            This project was a Node Server program that you can use to build a
-            website for your development team.
-          </p>
+        <div class="animate__animated animate__lightSpeedInRight card col-md-3">
+          <img src="./assets/teamprofile.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Profie Generator</h5>
+            <a
+              href="https://github.com/ChrisBoisjoli/Team_Profile_Generator"
+              target="_blank"
+            >
+              <i class="live fab fa-github fa-3x"></i>
+            </a>
+            <p class="card-text">
+              This project was a Node Server program that you can use to build a
+              website for your development team.
+            </p>
+          </div>
         </div>
-      </div>
-      <div class="animate__animated animate__lightSpeedInRight card col-md-3">
-        <img src="./assets/readmegen.png" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Readme Generator</h5>
-          <a
-            href="https://github.com/ChrisBoisjoli/README_Generator"
-            target="_blank"
-            ><i class="live fab fa-github fa-3x"></i
-          ></a>
-          <p class="card-text">
-            Utilizing the NPM package Inquirer this project will help users
-            create a Readme for their project.
-          </p>
+        <div class="animate__animated animate__lightSpeedInRight card col-md-3">
+          <img src="./assets/readmegen.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Readme Generator</h5>
+            <a
+              href="https://github.com/ChrisBoisjoli/README_Generator"
+              target="_blank"
+            >
+              <i class="live fab fa-github fa-3x"></i>
+            </a>
+            <p class="card-text">
+              Utilizing the NPM package Inquirer this project will help users
+              create a Readme for their project.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>);
+  );
 }
 
 export default Portfolio;
