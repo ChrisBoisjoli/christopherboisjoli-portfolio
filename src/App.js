d4ab9ee58e4ea8de9e1c6245b0a1b3c6d65b438e
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-05 14:53:07 -0500

    re adding scaffolding and starting over

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import  {Router} from "react";
+import logo from './logo.svg';
 import './App.css';
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <Router>
-      <Home></Home>
-      <Contact></Contact>
-      <Portfolio></Portfolio>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
