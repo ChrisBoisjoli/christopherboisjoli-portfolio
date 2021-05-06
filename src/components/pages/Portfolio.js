4b644f94956012b8c9785667dffb2dcc82b68866
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-06 09:09:16 -0500

    updated project portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,6 +4,7 @@ import Reel2Real from "../pages/images/reel2real.png";
 import Backroom from "../pages/images/backroom.png";
 import Readmegen from "../pages/images/readmegen.png";
 import Profilegenerator from "../pages/images/teamprofile.png";
+import Dailyrecreational from "../pages/images/dailyrecreational.png";
 function Portfolio() {
   return (
     <div className="container">
@@ -27,6 +28,28 @@ function Portfolio() {
             <p className="card-text">
               This is the second project that I worked on. I was responsible for
               many of the MVC back-end routes.
+            </p>
+          </div>
+        </div>
+        <div className="animate__animated animate__lightSpeedInRight card col-md-3">
+          <img src={Dailyrecreational} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Daily Recreational</h5>
+            <a
+              href="https://github.com/kdkleven/dailyRecreational"
+              target="_blank"
+            >Github Repo
+              <i className="live fab fa-github fa-3x"></i>
+            </a><br></br>
+            <a
+              href="https://kdkleven.github.io/dailyRecreational/"
+              target="_blank"
+            >Deployed site
+              <i className="live fab fa-github fa-3x"></i>
+            </a>
+            <p className="card-text">
+              This is the first project that I worked on. I was responsible for
+              many of the front-end layout.
             </p>
           </div>
         </div>
